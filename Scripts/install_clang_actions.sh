1c4a5073a8f86fdfb94e0c217c7116d449b3fb78
Arnd <Arnd_Github@SV-Zanshin.Com>
2020-12-05 12:27:37 +0100

    Added comments
    Added version number and comments

--- Scripts/install_clang_actions.sh
+++ Scripts/install_clang_actions.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
-
+################################################################################
+## Script to get software instaled and set up for doing "clang-format"        ##
+##                                                                            ##
+## Version Date       Developer  Description                                  ##
+## ======= ========== ========== ============================================ ##
+## 1.0.0   2020-12-05 SV-Zanshin Initial coding                               ##
+##                                                                            ##
+################################################################################
 set -e
 pip3 install clint
 sudo apt install -fy cppcheck clang-format-10
 if [ ! -f /usr/bin/clang-format ]; then
     sudo ln -s /usr/bin/clang-format-10 /usr/bin/clang-format
 fi
-## Copy the standard clang-format file to start directory but don't overwrite existing file
-echo GithubWorkspace is ${GITHUB_WORKSPACE}
+################################################################################
+## Copy the standard clang-format file to start directory but don't overwrite ##
+## the existing file if it already exists there. This ensures that a valid    ##
+## formatting file is used in any case                                        ##
+################################################################################
 cp -n ${GITHUB_WORKSPACE}/Common/clang-format/.clang-format ${GITHUB_WORKSPACE}/.clang-format
-ls -al ${GITHUB_WORKSPACE}
+echo The current version of clang-format is 
 clang-format --version

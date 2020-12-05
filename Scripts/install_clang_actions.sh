9549fea788f71d6d96dfe723382c3992452f7fda
Arnd <Arnd_Github@SV-Zanshin.Com>
2020-12-05 12:13:02 +0100

    Output version

--- Scripts/install_clang_actions.sh
+++ Scripts/install_clang_actions.sh
@@ -3,5 +3,6 @@
 set -e
 pip3 install clint
 sudo apt install -fy cppcheck clang-format
+clang-format --version
 ## Copy the standard clang-format file to start directory but don't overwrite existing file
 cp -n ${GITHUB_WORKSPACE}/Common/clang-format/.clang-format ${GITHUB_WORKSPACE}/.clang-format

84b116e96726cc09080ed164c0c70016063327fe
Arnd <Arnd_Github@SV-Zanshin.Com>
2020-12-05 11:35:23 +0100

    echo path

--- Scripts/install_clang_actions.sh
+++ Scripts/install_clang_actions.sh
@@ -7,4 +7,5 @@ if [ ! -f /usr/bin/clang-format ]; then
     sudo ln -s /usr/bin/clang-format-8 /usr/bin/clang-format
 fi
 ## Copy the standard clang-format file to start directory but don't overwrite existing file
+echo GithubWorkspace is ${GITHUB_WORKSPACE}
 cp -n ${GITHUB_WORKSPACE}/Common/clang-format/.clang-format ${GITHUB_WORKSPACE}/.clang-format

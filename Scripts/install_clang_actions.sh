86de4f5aa2651b5e3be7446d57dbb3285c22d69f
Arnd <Arnd_Github@SV-Zanshin.Com>
2020-12-05 12:02:12 +0100

    updated

--- Scripts/install_clang_actions.sh
+++ Scripts/install_clang_actions.sh
@@ -2,7 +2,8 @@
 
 set -e
 pip3 install clint
-sudo apt install -fy cppcheck clang-format-8
+#sudo apt install -fy cppcheck clang-format-8
+sudo apt install -fy cppcheck clang-format
 if [ ! -f /usr/bin/clang-format ]; then
     sudo ln -s /usr/bin/clang-format-8 /usr/bin/clang-format
 fi

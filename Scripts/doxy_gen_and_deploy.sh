f8935623523ffcb019c2a199e552a22e24845072
Arnd <Arnd_Github@SV-Zanshin.Com>
2020-12-06 09:01:50 +0100

    Moved files around

--- Scripts/doxy_gen_and_deploy.sh
+++ Scripts/doxy_gen_and_deploy.sh
@@ -35,7 +35,6 @@ __AUTHOR__="Jeroen de Bruijn, modified by ladyada"
 ################################################################################
 ##### Setup this script and get the current gh-pages branch.               #####
 echo 'Setting up the script...'
-env
 # Exit with nonzero exit code if anything fails
 set -e
 

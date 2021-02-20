716ebf5fc433dedb50b156f6a5d4f9577705e2c5
Arnd <Zanshin_Github@sv-zanshin.com>
2021-02-20 12:44:53 +0100

    Updated to install most recent version of clang-format

--- Scripts/install_clang_actions.sh
+++ Scripts/install_clang_actions.sh
@@ -13,7 +13,7 @@
 set -e
 pip3 install clint
 ##sudo apt install -fy cppcheck clang-format-10
-sudo apt install -fy cppcheck clang-format-12
+sudo apt install -fy cppcheck clang-format
 if [ ! -f /usr/bin/clang-format ]; then
     sudo ln -s /usr/bin/clang-format-10 /usr/bin/clang-format
 fi

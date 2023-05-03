12472e0f1840d06136d05685b58b88f1a5eca895
Arnd <Zanshin@zanshin-sailing.com>
2023-05-03 07:26:42 -0400

    Changed from Version 10 to 15

--- Scripts/install_clang_actions.sh
+++ Scripts/install_clang_actions.sh
@@ -7,13 +7,14 @@
 ##                                                                            ##
 ## Version Date       Developer  Description                                  ##
 ## ======= ========== ========== ============================================ ##
+## 1.010   2023-05-03 SV-Zanshin Updated from clang 10 to clang 15            ##
 ## 1.0.0   2020-12-05 SV-Zanshin Initial coding                               ##
 ##                                                                            ##
 ################################################################################
 set -e
 pip3 install clint
-sudo apt install -fy cppcheck clang-format-10
+sudo apt install -fy cppcheck clang-format-15
 if [ ! -f /usr/bin/clang-format ]; then
-    sudo ln -s /usr/bin/clang-format-10 /usr/bin/clang-format
+    sudo ln -s /usr/bin/clang-format-15 /usr/bin/clang-format
 fi
 cp -n ${GITHUB_WORKSPACE}/Common/clang-format/.clang-format ${GITHUB_WORKSPACE}/.clang-format

b4bb5c2412f0ccecb694bc3a98da002c4c943869
Arnd <Zanshin@zanshin-sailing.com>
2023-12-13 12:52:12 +0100

    Update install_arduino_cli.sh

--- Scripts/install_arduino_cli.sh
+++ Scripts/install_arduino_cli.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-
 set -e
 pip3 install pyserial
-python3 -m pip install packaging
 # make all our directories we need for files and libraries
 mkdir ${HOME}/.arduino15
 mkdir ${HOME}/.arduino15/packages
 mkdir ${HOME}/Arduino
 mkdir ${HOME}/Arduino/libraries
-
 # install arduino IDE
 export PATH=$PATH:$GITHUB_WORKSPACE/bin
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh -s 0.11.0 2>&1
